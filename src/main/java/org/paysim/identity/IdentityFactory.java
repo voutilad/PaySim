d1eabe3ae15944ece663a5ddd19f99d186090b55
Dave Voutila <voutilad@gmail.com>
2020-05-11 16:11:38 -0400

    Achievement Unlocked: use jFairy properly for once
    Turns out there was a major inefficiency in how jFairy was being used
    where the builder was being built over and over. This removes that
    redundancy.
    
    Also bumps the email extra digits up a tad. Feeling too lazy to make
    this a separate commit.
    
    Crank version, too.

--- src/main/java/org/paysim/identity/IdentityFactory.java
+++ src/main/java/org/paysim/identity/IdentityFactory.java
@@ -1,8 +1,10 @@
 package org.paysim.identity;
 
 import com.devskiller.jfairy.Bootstrap;
+import com.devskiller.jfairy.Fairy;
 import com.devskiller.jfairy.producer.company.Company;
 import com.devskiller.jfairy.producer.person.Person;
+import ec.util.MersenneTwisterFast;
 
 import java.util.HashSet;
 import java.util.Locale;
@@ -16,13 +18,13 @@ import java.util.Set;
 public class IdentityFactory {
     private final Set<String> ccnSet = new HashSet<>();
 
-    final private Bootstrap.Builder builder;
+    final private Fairy fairy;
 
     public IdentityFactory(int randomSeed) {
-        builder = Bootstrap.builder()
+        fairy = Bootstrap.builder()
                 .withRandomSeed(randomSeed)
                 .withLocale(Locale.US)
-                .withLocale(Locale.CANADA);
+                .withLocale(Locale.CANADA).build();
     }
 
     protected String addSuffixToEmail(String email, String suffix) {
@@ -32,41 +34,41 @@ public class IdentityFactory {
     }
 
     public ClientIdentity nextPerson() {
-        Person p = builder.build().person();
+        Person p = fairy.person();
 
         return new ClientIdentity(
                 getNextCreditCard(),
                 p.getFullName(),
-                addSuffixToEmail(p.getEmail(), p.getNationalIdentityCardNumber().substring(0, 2)),
+                addSuffixToEmail(p.getEmail(), p.getNationalIdentityCardNumber().substring(0, 3)),
                 p.getNationalIdentityCardNumber(),
                 p.getTelephoneNumber());
     }
 
     public BankIdentity nextBank() {
-        Person p = builder.build().person();
+        Person p = fairy.person();
         return new BankIdentity(getNextVAT(), String.format("Bank of %s", p.getLastName()));
     }
 
     public MerchantIdentity nextMerchant() {
-        Company c = builder.build().company();
+        Company c = fairy.company();
         return new MerchantIdentity(c.getVatIdentificationNumber(), c.getName());
     }
 
     public String getNextVAT() {
-        Company c = builder.build().company();
+        Company c = fairy.company();
         return c.getVatIdentificationNumber();
     }
 
     public String getNextCreditCard() {
-        String ccn = builder.build().creditCard().getCardNumber();
+        String ccn = fairy.creditCard().getCardNumber();
         while (!ccnSet.add(ccn)) {
-            ccn = builder.build().creditCard().getCardNumber();
+            ccn = fairy.creditCard().getCardNumber();
         }
         return ccn;
     }
 
     public String nextMerchantName() {
-        Company c = builder.build().company();
+        Company c = fairy.company();
         return c.getName();
     }
 }
--- src/test/java/org/paysim/identity/IdentityFactoryTest.java
+++ src/test/java/org/paysim/identity/IdentityFactoryTest.java
@@ -2,8 +2,23 @@ package org.paysim.identity;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.time.Duration;
+import java.util.HashSet;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class IdentityFactoryTest {
+
+    @Test
+    void successiveCallsCreateNewPeople() {
+        IdentityFactory factory = new IdentityFactory(1);
+        ClientIdentity identity1 = factory.nextPerson();
+        ClientIdentity identity2 = factory.nextPerson();
+        Assertions.assertNotEquals(identity1, identity2);
+        Assertions.assertNotEquals(identity1.name, identity2.name);
+        Assertions.assertNotEquals(identity1.id, identity2.id);
+    }
 
     @Test
     void generatedEmailsAddRandomDigits() {
@@ -20,11 +35,34 @@ public class IdentityFactoryTest {
         int addrlen = parts[0].length();
 
         try {
-            int suffix = Integer.valueOf(parts[0].substring(addrlen - 2));
+            int suffix = Integer.valueOf(parts[0].substring(addrlen - 3));
             Assertions.assertTrue(suffix > -1 && suffix < 100,
                     suffix + " should be double digits");
         } catch (Exception e) {
             Assertions.fail("didn't find digits in email: " + email);
         }
+    }
+
+    @Test
+    void collisionTest() {
+        System.out.println("Starting collision test...");
+        final long start = System.currentTimeMillis();
+        final int max = 1_000_000;
+        final AtomicInteger collisions = new AtomicInteger(0);
+        final IdentityFactory factory = new IdentityFactory(1);
+        final HashSet<String> ccSet = new HashSet<>(max);
+
+        Assertions.assertTimeout(Duration.ofSeconds(15), () -> {
+            while (ccSet.size() < max) {
+                String cc = factory.getNextCreditCard();
+                while (!ccSet.add(cc)) {
+                    collisions.incrementAndGet();
+                    cc = factory.getNextCreditCard();
+                }
+            }
+        });
+        final long finish = System.currentTimeMillis();
+        System.out.println("Finished in " + (finish - start) + " millis");
+        System.out.println("collisions: " + collisions.get());
     }
 }
